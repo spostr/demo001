1dd50e882ead6140807092dcc60e7b98244f09f3
panqing <panqing@localhost.localdomain>
2022-09-12 15:07:15 +0800

    1. update spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#include <spdlog/spdlog.h>
 
 int main() {
-    std::cout << "hello world" << std::endl;
-    return 0;     
+    spdlog::set_level(spdlog::level::debug);
+    spdlog::set_pattern("%Y-%m-%d %H:%M:%S.%e|pid:%P|tid:%t|%s:%#|%!()|%l >> %v");
+
+    SPDLOG_DEBUG("hello world");
+    return 0;
 }
